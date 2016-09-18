28d27a514a1cf547a36a641d98ff3094b78cc455
code-for-coffee <jtraverjr@gmail.com>
2016-09-18 14:03:35 -0500

    added models

---
+++ SharpQuotes/Models/User.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using BCrypt;
+
+namespace SharpQuotes
+{
+	public class User
+	{
+		public User(string name, string email, string password)
+		{
+			this.passwordHash = BCrypt.Net.BCrypt.HashPassword(password);
+		}
+		private string passwordHash;
+		public string username { get; set; }
+		public string email { get; set; }
+		public Boolean comparePassword(string userInput)
+		{
+			if (BCrypt.Net.BCrypt.Verify(userInput, this.passwordHash) == true) {
+				return true;
+			}
+			return false;
+		}
+	}
+}
+
