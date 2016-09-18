a0ae3f29595a30caf293ce0c6b40ffbdb9e806b5
code-for-coffee <jtraverjr@gmail.com>
2016-09-18 14:30:11 -0500

    added meta-deta for model classes & methods

--- SharpQuotes/Models/Quote.cs
+++ SharpQuotes/Models/Quote.cs
@@ -1,11 +1,22 @@
 ﻿using System;
 namespace SharpQuotes
 {
+	/// <summary>
+	/// Quote models.
+	/// </summary>
 	public class Quote
 	{
+		/// <summary>
+		/// Initializes a new instance of the <see cref="T:SharpQuotes.Quote"/> class.
+		/// </summary>
 		public Quote()
 		{
 		}
+		/// <summary>
+		/// Initializes a new instance of the <see cref="T:SharpQuotes.Quote"/> class.
+		/// </summary>
+		/// <param name="author">Author.</param>
+		/// <param name="text">Text.</param>
 		public Quote(string author, string text)
 		{
 			this.author = author;
--- SharpQuotes/Models/User.cs
+++ SharpQuotes/Models/User.cs
@@ -3,8 +3,17 @@ using BCrypt;
 
 namespace SharpQuotes
 {
+	/// <summary>
+	/// User model that utilizes BCrypt for password hashing.
+	/// </summary>
 	public class User
 	{
+		/// <summary>
+		/// Initializes a new instance of the <see cref="T:SharpQuotes.User"/> class.
+		/// </summary>
+		/// <param name="name">Name.</param>
+		/// <param name="email">Email.</param>
+		/// <param name="password">Password.</param>
 		public User(string name, string email, string password)
 		{
 			this.username = name;
@@ -14,6 +23,11 @@ namespace SharpQuotes
 		private string passwordHash;
 		public string username { get; set; }
 		public string email { get; set; }
+		/// <summary>
+		/// Compares an input password to the model's hash.
+		/// </summary>
+		/// <returns><c>true</c>, if password was compared, <c>false</c> otherwise.</returns>
+		/// <param name="userInput">User input.</param>
 		public Boolean comparePassword(string userInput)
 		{
 			if (BCrypt.Net.BCrypt.Verify(userInput, this.passwordHash) == true) {
